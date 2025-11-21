970d8bd4889119a431037fc2ca305e75b364be8a
Sarat W. <wsarat@slvusd.org>
2025-11-21 13:34:39 -0800

    fix

--- webmin.c
+++ webmin.c
@@ -8,8 +8,8 @@ static httpd_handle_t webmin_server = NULL;
 
 static const char *TAG = "webmin";
 
-extern const uint8_t index_html_start[] asm("_binary_index_html_start")
-extern const uint8_t index_html_end[] asm("_binary_index_html_end")
+extern const uint8_t index_html_start[] asm("_binary_index_html_start");
+extern const uint8_t index_html_end[] asm("_binary_index_html_end");
 
 #define HTTPD_401      "401 UNAUTHORIZED"           /*!< HTTP Response 401 */
 
